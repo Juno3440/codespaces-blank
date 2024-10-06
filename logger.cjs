efb6dc1ad6569d215089136ee9c035fe56eeba0f
Juno3440 <thaburrito922@gmail.com>
2024-10-06 14:58:27 +0000

    I dont even know

--- logger.cjs
+++ logger.cjs
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-const logFilePath = path.join(__dirname, 'logs.txt');
+const logDir = path.join(__dirname, 'logs');
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+const logFilePath = path.join(logDir, 'logs.txt');
 
 function logMessage(message) {
   const timestamp = new Date().toISOString();
